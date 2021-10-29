fa2a8b1975fe59238746e2421eb0c99258c2745d
Jérôme Coupé <jercoupe@gmail.com>
2021-10-29 09:22:27 +0200

    Add delays + better comments

--- README.md
+++ README.md
@@ -4,13 +4,21 @@
 - Use intersectionObserver
 - Elements displayed on pages if JS is not supported
 
-# Usage
+[Demo](https://jeromecoupe.github.io/onscroll_css_animations/)
+
+## Usage
+
+### Basic usage
 
 Add a `data-scrollanimation="true"` and either a `data-scroll-animation-type="slideFromBottom"`,  `data-scroll-animation-type="slideFromLeft"` or `data-scroll-animation-type="slideFromRight"` data attribute to elements you wish to animate.
 
-- All animations classes are set via JS
+- Animation classes are set via JS
 - All animations are switched to running when animated elements are displayed in the viewport. If elements are in the viewport when the page is loaded, animations will be triggered immediately.
 - Adding new animations types using CSS is really easy.
+
+### Utilities
+
+Delays can be specified using `data-scrollanimation-delay=[valid CSS delay]"`. The following data attribute will delay an animation by 0.5 seconds: `data-scrollanimation-delay=0.5s]"`. Delays are added as inline styles via JS.
 
 ## Installation
 
--- js/modules/scroll-animations.js
+++ js/modules/scroll-animations.js
@@ -5,14 +5,14 @@ const runningAttribute = "data-scrollanimation-running";
 const animatedElements = document.querySelectorAll("[data-scrollanimation]");
 
 /**
- * IntersectionObserver détecte des collisions entre un elment racine et un élément cible. Le mécanisme est analogue à celui de deux puces RFID attachées aux éléments, dont on détecte la collision
+ * IntersectionObserver détecte des collisions entre un élément racine et un élément cible. Le mécanisme est analogue à celui de deux puces RFID attachées aux éléments, dont on détecte la collision
  *
  * - racine: définie dans les options d'IntersectionObserver (root)
  * - cible: le ou les éléments auxquels sont attachés la "puce RFID" d'IntersectionObserver
  */
 const observerOptions = {
-  // root: défini le viewport comme élément racine des collisions
-  // threshold 0.5: 50% de l'objet doit être en collision avec le viewport
+  // root: défini le viewport comme élément racine
+  // threshold 0.5: 50% de l'objet cible doit être en collision avec l'élément racine
   root: null,
   threshold: 0.5,
 };
@@ -21,16 +21,17 @@ function init() {
   if (!"IntersectionObserver" in window) return;
   /**
    * Création d'observer: prend une fonction de callback et des options
-   * - function callback: on regarde si un des item intersecte
+   *
+   * - callback (function): on regarde si un des item intersecte
    *   si oui on start l'animation et on supprime l'observer
    * - option: cf supra (racine et threshold)
    */
-  const observer = new IntersectionObserver(function (observedItems) {
-    observedItems.forEach(function (item) {
-      if (item.isIntersecting) {
+  const observer = new IntersectionObserver(function (observerEntries) {
+    observerEntries.forEach(function (entry) {
+      if (entry.isIntersecting) {
         // attention intersection observer ne s'applique pas directement à des éléments (comme une puce RFID). Si vous voulez cibler l'élément auquel est attachée la puce RFID, il faut utiliser el.target
-        item.target.setAttribute(runningAttribute, true);
-        observer.unobserve(item.target);
+        entry.target.setAttribute(runningAttribute, true);
+        observer.unobserve(entry.target);
       }
     });
   }, observerOptions);
@@ -40,8 +41,12 @@ function init() {
    * attacher un observer à chaque element
    */
   animatedElements.forEach(function (el) {
+    // generate animation classes
     let animationClass = el.dataset.scrollanimationType;
     el.classList.add(animationClass);
+    // generate delays (inline styles)
+    let delay = el.dataset.scrollanimationDelay;
+    el.style.animationDelay = delay;
     observer.observe(el);
   });
 }
