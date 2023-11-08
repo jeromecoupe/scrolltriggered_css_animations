b57a303b5179512f33f07c8b97d45274598948d2
Jérôme Coupé <jercoupe@gmail.com>
2023-11-08 19:46:00 +0100

    better architecture and tests

--- js/modules/scroll-animations.js
+++ js/modules/scroll-animations.js
@@ -1,36 +1,67 @@
-// data attibute pour dire que l'animation devient active
-const runningAttribute = "data-scrollanimation-running";
-
-// element à animer
+// elements à animer
 const animatedElements = document.querySelectorAll("[data-scrollanimation]");
 
+// observer threshold
+const observerThreshold = 0.5;
+
 /**
- * IntersectionObserver détecte des collisions entre un élément racine et un élément cible. Le mécanisme est analogue à celui de deux puces RFID attachées aux éléments, dont on détecte la collision
- *
- * - racine: définie dans les options d'IntersectionObserver (root)
+ * IntersectionObserver détecte des collisions entre un élément racine et un élément cible.
+ * Le mécanisme est analogue à celui de deux puces RFID attachées aux éléments, dont on détecte la collision
+ * - racine: définie dans les options d'IntersectionObserver (root = viewport)
  * - cible: le ou les éléments auxquels sont attachés la "puce RFID" d'IntersectionObserver
  */
 const observerOptions = {
-  // root: défini le viewport comme élément racine
-  // threshold 0.5: 50% de l'objet cible doit être en collision avec l'élément racine
+  // root: null - défini le viewport comme élément racine
+  // threshold 0.5 - 50% de l'objet cible doit être en collision avec l'élément racine
   root: null,
-  threshold: 0.5,
+  threshold: observerThreshold,
 };
+
+/**
+ * Add animation classes to element
+ * @param {DOM node} el - target DOM node
+ */
+function addAnimationClasses(el) {
+  // prepare values
+  let animationClass = el.dataset.scrollanimationType;
+  let delay = el.dataset.scrollanimationDelay;
+  // apply values
+  el.classList.add(animationClass);
+  el.style.animationDelay = delay;
+  el.style.animationPlayState = "paused";
+}
+
+/**
+ * Change the animation play state for element
+ * @param {DOM node} el - target DOM node
+ */
+function playAnimation(el) {
+  el.style.animationPlayState = "running";
+}
 
 function init() {
   if (!"IntersectionObserver" in window) return;
+
   /**
    * Création d'observer: prend une fonction de callback et des options
    *
-   * - callback (function): on regarde si un des item intersecte
-   *   si oui on start l'animation et on supprime l'observer
+   * - callback (function):
+   *    - si intersection ratio > threshold: on supprime l'observer et on ne fait rien
+   *    - si intersetcion ratio < threshold: on appliquer les classes d'animation et on pause
+   *    - si intersection: on passe l'animation en play et on supprime l'observer
    * - option: cf supra (racine et threshold)
    */
   const observer = new IntersectionObserver(function (observerEntries) {
+    // attention intersection observer ne s'applique pas directement à des éléments (comme une puce RFID). Si vous voulez cibler l'élément auquel est attachée la puce RFID, il faut utiliser entry.target
     observerEntries.forEach(function (entry) {
+      if (entry.intersectionRatio > observerThreshold) {
+        observer.unobserve(entry.target);
+      }
+      if (entry.intersectionRatio < observerThreshold) {
+        addAnimationClasses(entry.target);
+      }
       if (entry.isIntersecting) {
-        // attention intersection observer ne s'applique pas directement à des éléments (comme une puce RFID). Si vous voulez cibler l'élément auquel est attachée la puce RFID, il faut utiliser el.target
-        entry.target.setAttribute(runningAttribute, true);
+        playAnimation(entry.target);
         observer.unobserve(entry.target);
       }
     });
@@ -41,12 +72,6 @@ function init() {
    * attacher un observer à chaque element
    */
   animatedElements.forEach(function (el) {
-    // generate animation classes
-    let animationClass = el.dataset.scrollanimationType;
-    el.classList.add(animationClass);
-    // generate delays (inline styles)
-    let delay = el.dataset.scrollanimationDelay;
-    el.style.animationDelay = delay;
     observer.observe(el);
   });
 }
